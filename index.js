c38b745f48cd840215a20d5fe9a6869c813c021c
leila <laila.ahmed.2645@gmail.com>
2026-01-17 20:03:13 +0300

    scobe-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// helpers.js
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
